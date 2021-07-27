a64710c303680d75654fa76379f0ab9051cbefd3
mahmutsami <mahmutsami106@hotmail.com>
2021-07-27 15:15:21 +0300

    Integration - BusReservation system app class were created.

---
+++ src/main/java/com/reservationsteps/springboot/msy/brs/BusReservationSystemApplication.java
@@ -0,0 +1,9 @@
+package com.reservationsteps.springboot.msy.brs;
+
+import org.springframework.boot.SpringApplication;
+
+public class BusReservationSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusReservationSystemApplication.class, args);
+    }
+}
